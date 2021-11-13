462c50071ff487b29b94e7cfbc3683e43ebdab6c
shubhkumar456 <shubhkumar456@gmail.com>
2021-11-13 23:54:34 +0530

    list controller

---
+++ src/main/java/com/example/Dear/comrade/Project/pathvariable/ListControllerWith_params.java
@@ -0,0 +1,42 @@
+package com.example.Dear.comrade.Project.pathvariable;
+
+import com.sun.javafx.collections.MappingChange;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ListControllerWith_params {
+
+    List <String> namelist = new ArrayList<>();
+
+       @RequestMapping("/add")
+        public  String add() {
+           namelist.add("hello world");
+           namelist.add("hello future");
+           namelist.add("hello past ");
+           return "params added successfully";
+       }
+
+       @RequestMapping("/getter")
+       public List<String> getNamelist(){
+           return namelist;
+       }
+
+       @RequestMapping("/uptodate")
+       public String update(){
+           namelist.remove(2);
+           namelist.add(2,"Hello welcome to the past");
+           return "object updated successfully";
+       }
+
+       @RequestMapping("/rem")
+       public String remove(){
+           namelist.remove(2);
+           return "object remove successfully";
+       }
+
+}
